3e1ff78ed417920ff8aa1e12dfbfdae37945bad1
kentaroy47 <meathouse47@gmail.com>
2019-09-03 21:54:06 +0900

    init

--- fc.py
+++ fc.py
@@ -68,15 +68,50 @@ Split_train_data = x_train_data.reshape([int(x_train_data.shape[0]/D), x_train_d
 Split_test_data=x_test_data.reshape([int(x_test_data.shape[0]/D), x_test_data.shape[1]*D])
 
 Split_train_data_x=Split_train_data[0:-1,:]
-Split_train_data_y=Split_train_data[1::,:]
+Split_train_data_y=Split_train_data[1:,:]
 
 Split_test_data_x=Split_test_data[0:-1,:]
-Split_test_data_y=Split_test_data[1::,:]
+Split_test_data_y=Split_test_data[1:,:]
 
 img_rows = D
-Split_test_data = Split_test_data.reshape(Split_test_data.shape[0], img_rows, 1 , 1)
-Split_train_data = Split_train_data.reshape(Split_train_data.shape[0], img_rows, 1 , 1)
-Split_test_data_y = Split_test_data.reshape(Split_test_data.shape[0], img_rows)
-Split_train_data_y = Split_train_data.reshape(Split_train_data.shape[0], img_rows)
+#Split_test_data = Split_test_data.reshape(Split_test_data.shape[0], img_rows, 1 , 1)
+#Split_train_data = Split_train_data.reshape(Split_train_data.shape[0], img_rows, 1 , 1)
+#Split_test_data_y = Split_test_data_y.reshape(Split_test_data.shape[0], img_rows)
+#Split_train_data_y = Split_train_data_y.reshape(Split_train_data.shape[0], img_rows)
 
 input_shape = (img_rows, 1, 1)
+
+class anomaly_dataset(torch.utils.data.Dataset):
+    def __init__(self, data, target):
+        self.data = data
+        self.target = target
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        return self.data[index], self.target[index]
+    
+train_dataset = anomaly_dataset(Split_train_data_x, Split_train_data_y)
+val_dataset = anomaly_dataset(Split_test_data_x, Split_test_data_y)
+
+# データローダーの作成
+
+batch_size = 4
+
+train_dataloader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=batch_size, shuffle=True)
+
+val_dataloader = torch.utils.data.DataLoader(
+    val_dataset, batch_size=batch_size, shuffle=False)
+
+# 辞書型変数にまとめる
+dataloaders_dict = {"train": train_dataloader, "val": val_dataloader}
+
+# 動作の確認
+batch_iterator = iter(dataloaders_dict["val"])  # イタレータに変換
+images, targets = next(batch_iterator)  # 1番目の要素を取り出す
+print(images.size())  # torch.Size([4, 3, 300, 300])
+print(len(targets))
+print(targets[0].shape)  # ミニバッチのサイズのリスト、各要素は[n, 5]、nは物体数
+
